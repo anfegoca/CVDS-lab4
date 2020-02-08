5d5a531248a018e65910b710c0a75c30bff5dc8c
unknown <andres.gonzalez-ca@labinfo.is.escuelaing.edu.co>
2020-02-08 09:57:53 -0500

    Implementación pruebas

---
+++ src/test/java/hangman/test/GameScoreTest.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman.test;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author andres.gonzalez-ca
+ */
+public class GameScoreTest {
+    
+    
+    /**
+     * correctCount < 0    ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScoreOriginal() {
+        
+        
+    }
+    /**
+     * correctCount > 0    Integer
+     */
+    @Test
+    public void calculateScoreOriginal2() {
+        
+        
+    }
+    
+    /**
+     * incorrectCount < 0  ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScoreOriginal3() {
+        
+        
+    }
+
+    /**
+     * incorrectCount > 0  Integer
+     */
+    @Test
+    public void calculateScoreOriginal4() {
+        
+    }
+    /**
+     * correctCount < 0   ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScorePower() {
+        
+        
+    }
+    /**
+     * correctCount > 0 
+     * 
+     */
+    @Test
+    public void calculateScorePower2() {
+        
+        
+    }
+    
+    /**
+     *
+     * incorrectCount < 0  ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScorePower3() {
+        
+        
+    }
+
+    /**
+     * incorrectCount > 0 
+     */
+    @Test
+    public void calculateScorePower4() {
+        
+    }
+    /**
+     * correctCount < 0   ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScoreBonusl() {
+        
+        
+    }
+    /**
+     * correctCount > 0  Integer
+     */
+    @Test
+    public void calculateScoreBonus2() {
+        
+        
+    }
+    
+    /**
+     * incorrectCount < 0  ExceptionValorInvalido
+     */
+    @Test
+    public void calculateScoreBonus3() {
+        
+        
+    }
+
+    /**
+     * incorrectCount > 0  Integer
+     */
+    @Test
+    public void calculateScoreBonus4() {
+        
+    }
+
+}
